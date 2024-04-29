5c0b3107ab774c798c1670ddd9816ff3d23bbfa3
RahulUchihaa <rahu.reddy@agileidc.com>
2024-04-29 15:34:10 +0530

    PaymentTable changes

--- App/Components/Reservations/Payment.tsx
+++ App/Components/Reservations/Payment.tsx
@@ -1,14 +1,11 @@
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
-import { TextInput } from 'react-native-paper';
+import { Modal, Portal, TextInput } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/MaterialIcons';
 import { useSelector } from 'react-redux';
 import Colors from '../../Constants/Colors';
 import useDispatch from '../../Hooks/useDispatch';
@@ -19,9 +16,8 @@ import {
   fetchStatus,
 } from '../../Redux/Reducers/ReservationDetailsReducer';
 import { RootState } from '../../Redux/Store';
-import moment from 'moment';
 
-const Payment = ({item}: any) => {
+const Payment = ({ item }: any) => {
   const [type, setType] = useState('');
   const [method, setMethod] = useState('');
   const [value, setValue] = useState('');
@@ -30,6 +26,7 @@ const Payment = ({item}: any) => {
   const [date, setDate] = useState('');
   const [status, setSelectedStatus] = useState('Not paid');
   const statusOptions = ['Paid', 'Pending'];
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const handleDateConfirm = (newdate: Date) => {
     // console.log("Date === ",date)
@@ -94,19 +91,18 @@ const Payment = ({item}: any) => {
     }
   };
 
-  const {paymentHistory, loading} = useSelector(
+  const { paymentHistory, loading } = useSelector(
     (state: RootState) => state.fetchPaymentReducer,
   );
 
-  const{defaultCurrency} = useSelector(
+  const { defaultCurrency } = useSelector(
     (state: RootState) => state.fleetFetchDefaultCurrencyReducer,
-  )
+  );
 
   // const statusColor = getStatusColor(
   //   paymentHistory?.reservation_payment?.status,
   // );
 
-  
   useEffect(() => {
     if (isMounted()) {
       dispatch(fetchPayment(item?.reservation?.id));
@@ -134,103 +130,211 @@ const Payment = ({item}: any) => {
     setShowOptions(false); // Hide options after selecting
   };
 
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      {paymentHistory?.reservation_payment?.map(
-        (
-          payment: {
-            status: string;
-            type:any;
-            date:any;
-            method:any;
-            value: any;
-            id: any;
-          },
-          index: React.Key | null | undefined,
-        ) => (
-          <View style={styles.paymentItem} key={payment.id}>
-            <Text style={styles.paymentDate}>{moment(payment.date).format('DD-MM-YYYY')}</Text>
-            <View style={[styles.paymentMethod]}>
-              <Text style={[styles.paymentDate,{ marginLeft: 8 }]}>{payment.method}</Text>
-            </View>
-            <Text style={styles.amount}>{`${payment.value} ${defaultCurrency.parameter_value}`}</Text>
-            <View style={{width:70}}>
-              {/* <Text>Select Status:</Text> */}
-              <TouchableOpacity
-                onPress={() => toggleOptions(payment.id)}
-                style={[styles.selectedOption,{backgroundColor: getStatusColor(payment.status)}]}>
-                <Text style={{color: Colors.Iconwhite}}>{payment.status}</Text>
-              </TouchableOpacity>
-              {selectedRow === payment.id && showOptions === true && (
-                <View style={[styles.optionsContainer]}>
-                  {statusOptions.map((status, index) => (
-                    <TouchableOpacity
-                      key={index}
-                      onPress={() => handleSelectStatus(status, payment.id)}
-                      style={{padding: 10, backgroundColor: getStatusColor(status)}}>
-                      <Text style={{color: Colors.Iconwhite,textAlign:'center'}}>{status}</Text>
-                    </TouchableOpacity>
-                  ))}
-                </View>
-              )}
-            </View>
-          </View>
-        ),
-      )}
-
-      <View style={styles.formContainer}>
-        <TextInput
-          label="Payment type"
-          value={type}
-          onChangeText={value => {
-            setType(value);
-          }}
-          mode="outlined"
-          style={styles.input}
-          textColor={Colors.black}
-          // right={<TextInput.Icon icon="menu-down" />}
-        />
-        <View>
-          <TouchableOpacity
-            style={[
-              styles.input,
-              { borderRadius: 3, borderWidth:1, justifyContent: 'space-between',flexDirection:'row',alignItems:'center',paddingHorizontal:15},
-            ]}
-            onPress={() => setDatePickerVisibility(true)}>
-            <Text style={{color: Colors.black}}>
-              {selectedDate ? selectedDate.toDateString() : 'Select Date'}
-            </Text>
-            <Icon name='calendar' size={20} color={Colors.black}/>
-          </TouchableOpacity>
-          
-        </View>
-
-        <TextInput
-          label="Payment method"
-          value={method}
-          onChangeText={value => {
-            setMethod(value);
-          }}
-          mode="outlined"
-          style={styles.input}
-          textColor={Colors.black}
-          // right={<TextInput.Icon icon="menu-down" />}
-        />
-        <TextInput
-          label="Amount"
-          value={value}
-          onChangeText={text => setValue(text)}
-          mode="outlined"
-          style={styles.input}
-          textColor={Colors.black}
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={addPayment}>
+      <View style={{ justifyContent: 'center', alignItems: 'flex-end' }}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: Colors.primary,
+              margin: 20,
+              width: 110
+            },
+          ]}
+          onPress={() => setIsModalVisible(true)}>
           <Text style={styles.buttonText}>Add Payment</Text>
         </TouchableOpacity>
       </View>
+      <View style={{ paddingLeft: 10, paddingRight: 10 }}>
+        <View style={styles.row}>
+          <Text style={styles.cell}>
+            Date
+          </Text>
+          <Text style={styles.cell}>
+            Type
+          </Text>
+          <Text style={styles.cell}>
+            Amount
+          </Text>
+          <Text style={styles.cell}>
+            Status
+          </Text>
+        </View>
+      </View>
+
+      {
+        paymentHistory?.reservation_payment.length > 0 ?
+          (
+            <>
+              {paymentHistory?.reservation_payment?.map(
+                (
+                  payment: {
+                    status: string;
+                    type: any;
+                    date: any;
+                    method: any;
+                    value: any;
+                    id: any;
+                  },
+                  index: React.Key | null | undefined,
+                ) => (
+                  <View style={styles.row} key={payment.id}>
+                    <Text style={[styles.paymentDate, styles.cell]}>
+                      {moment(payment.date).format('DD-MM-YYYY')}
+                    </Text>
+                    <Text style={[styles.paymentDate, styles.cell]}>
+                      {payment.method}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.amount, styles.cell
+                      ]}>{`${payment.value} ${defaultCurrency.parameter_value}`}</Text>
+                    <View style={styles.cell}>
+                      {/* <Text>Select Status:</Text> */}
+                      <TouchableOpacity
+                        onPress={() => toggleOptions(payment.id)}
+                        style={[
+                          styles.selectedOption,
+                          { backgroundColor: getStatusColor(payment.status) },
+                        ]}>
+                        <Text style={{ color: Colors.Iconwhite }}>{payment.status}</Text>
+                      </TouchableOpacity>
+                      {selectedRow === payment.id && showOptions === true && (
+                        <View style={[styles.optionsContainer]}>
+                          {statusOptions.map((status, index) => (
+                            <TouchableOpacity
+                              key={index}
+                              onPress={() => handleSelectStatus(status, payment.id)}
+                              style={{
+                                padding: 10,
+                                backgroundColor: getStatusColor(status),
+                              }}>
+                              <Text
+                                style={{ color: Colors.Iconwhite, textAlign: 'center' }}>
+                                {status}
+                              </Text>
+                            </TouchableOpacity>
+                          ))}
+                        </View>
+                      )}
+                    </View>
+                  </View>
+                ),
+              )}
+            </>
+          )
+          :
+          (
+            <View style={{justifyContent:'center',alignItems:'center',marginTop:20}}>
+              <Text style={{fontWeight:'bold',color:Colors.black}}>No data found</Text>
+            </View>
+          )
+      }
+      {/* Modal Popup for Adding payment */}
+      <Portal>
+        <Modal
+          visible={isModalVisible}
+          contentContainerStyle={styles.modalContainer}>
+              <View
+            style={{
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flexDirection: 'row',
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: 'bold',
+                color: Colors.black,
+              }}>
+              Add Payment
+            </Text>
+            <TouchableOpacity onPress={closeModal}>
+              <Icon2 name={'cancel'} size={25} color={Colors.red} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.formContainer}>
+            <TextInput
+              label="Payment type"
+              value={type}
+              onChangeText={value => {
+                setType(value);
+              }}
+              mode="outlined"
+              style={styles.input}
+              textColor={Colors.black}
+            // right={<TextInput.Icon icon="menu-down" />}
+            />
+            <View>
+              <TouchableOpacity
+                style={[
+                  styles.input,
+                  {
+                    borderWidth: 0.5,
+                    borderRadius: 3,
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingHorizontal: 15,
+                  },
+                ]}
+                onPress={() => setDatePickerVisibility(true)}>
+                <Text style={{ color: Colors.black }}>
+                  {selectedDate ? selectedDate.toDateString() : 'Select Date'}
+                </Text>
+                <Icon name="calendar" size={20} color={Colors.black} />
+              </TouchableOpacity>
+            </View>
+
+            <TextInput
+              label="Payment method"
+              value={method}
+              onChangeText={value => {
+                setMethod(value);
+              }}
+              mode="outlined"
+              style={styles.input}
+              textColor={Colors.black}
+            // right={<TextInput.Icon icon="menu-down" />}
+            />
+            <TextInput
+              label="Amount"
+              value={value}
+              onChangeText={text => setValue(text)}
+              mode="outlined"
+              style={styles.input}
+              textColor={Colors.black}
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: Colors.primary }]}
+                onPress={addPayment}>
+                <Text style={styles.buttonText}>Submit</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, { backgroundColor: Colors.red }]}
+                onPress={() => {
+                  setIsModalVisible(false);
+                }}>
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </Portal>
+
       {/* Date Time Picker Modal */}
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
@@ -289,8 +393,7 @@ const styles = StyleSheet.create({
   paymentMethod: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    width:'10%'
+    justifyContent: 'center',
   },
   statusButton: {
     borderWidth: 0.1,
@@ -315,12 +418,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
     color: '#333',
-    width:'25%',
-    textAlign:'right'
+    textAlign: 'right',
   },
   formContainer: {
-    paddingHorizontal: 20,
-    paddingTop:10
+    paddingHorizontal: 0,
+    paddingTop: 10,
   },
   input: {
     marginBottom: 10,
@@ -354,17 +456,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   button: {
-    backgroundColor: Colors.primary,
     padding: 10,
     borderRadius: 5,
     marginRight: 20,
   },
   selectedOption: {
     padding: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
     borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
-  }, 
+  },
   optionsContainer: {
     marginTop: 10,
     backgroundColor: 'white',
@@ -377,9 +480,33 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 10,
-    width:80,
   },
   option: {
     padding: 10,
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    margin: 20,
+    padding: 15, // Semi-transparent background
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: Colors.primary,
+    padding: 10,
+    borderRadius: 3
+  },
+  cell: {
+    flex: 1,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: Colors.Iconwhite
+  },
+  headerCell: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 15,
   },
 });
